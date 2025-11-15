a8db48362c7c436d94abcc73f0c0b1bff4a367c9
jamaludin1021 <rendradewaputra@gmail.com>
2025-11-15 09:28:14 +0700

    Update user prompt and improve script messages

--- script-all-update.sh
+++ script-all-update.sh
@@ -1,22 +1,33 @@
 #!/usr/bin/bash
 
-user="ya"
+user="free user"
 while [ "$username" != "$user" ]
 do
- read -p "apa kamu yakin ingin menggunakan tools ini? [y]: " username
+ read -p "masukan username: " username
  done
- echo -e "memulai tools...:"
+ echo -e "memverikasi user:"
  sleep 2
  echo -e "ꈍ◡ꈍ wait"
  sleep 2 
  echo -e "...
  "
  sleep 2
- echo -e " ദ്ദി˶˃ ᵕ ˂ )✧ tools berhasil dijalankan"
+ echo -e " ദ്ദി˶˃ ᵕ ˂ )✧ verivikasi berhasil"
  sleep 2 
  clear
+ echo -e "script ini dibuat oleh: jamaludin"
+ sleep 1 
+ echo -e "
  
- #warna
+ "
+ echo -e "DETAILS"
+ echo -e "
+ "
+echo -e "script use bash"
+echo -e "script yang mungkin berguna untuk menginstall update awal"
+sleep 3 
+clear
+#warna
 default="\033[00m"
 merah="\033[31m"
 hijau="\033[32m"
@@ -25,22 +36,68 @@ biru="\033[34m"
 ungu="\033[35m"
 birumuda="\033[36m"
 putih="\033[37m"
-pink="\033[38m"
 menu() {
-  echo -e "
-  !!PESAN DARI DEWXMAMAL!!
-  
-  "
+  clear 
+echo -e $kuning"berhasil tersambung"
+ sleep 3
+ clear
+  echo -e $hijau"halo kontol"
+  sleep 6
+  clear
+  echo -e $biru"nungguin ya"
   sleep 3
-  echo -e $kuning"lebih baik sadar diri"
+  clear
+  echo -e $ungu"selamat datang di program gabut yang dibuat oleh
+  D.I.T | jamaludin
+▇◤▔▔▔▔▔▔▔◥▇
+▇▏◥▇◣┊◢▇◤▕▇
+▇▏▃▆▅▎▅▆▃▕▇
+▇▏╱▔▕▎▔▔╲▕▇
+▇◣◣▃▅▎▅▃◢◢▇
+▇▇◣◥▅▅▅◤◢▇▇
+▇▇▇◣╲▇╱◢▇▇▇
+  "
+ sleep 3
+  clear
+  echo -e $hijau"ty to"
   echo -e "
-  █▄░▄█ █▀ ▄▀▄ █░░░█ █░░
-  █░█░█ █▀ █░█ █░█░█ █░░
-  ▀░░░▀ ▀▀ ░▀░ ░▀░▀░ ▀▀▀
+  ˚∧＿∧ 　+　　—̳͟͞͞💗
+  ( •‿• )つ —̳͟͞͞ 💗　　 —̳͟͞͞💗 +
+   つ　 <　　　　—̳͟͞͞💗
+   ｜　 _つ　 + —̳͟͞͞💗　　 —̳͟͞͞💗 ˚
+   し´
+Love sent by jamaludin
   "
+  echo -e $hijau"D.I.T|TEAM"
   sleep 2 
-  echo -e $merah"dari pada sakit hati
+  echo -e "
+➖🟩🟩🟩
+➖🟩🟩⬛
+➖🟩🟩🟩
+➖➖🟦➖⬜⬜
+➖➖🟦🟩⬜⬛
+➖➖🟦➖⬜⬜🟧
+⬜⬜⬜🟦⬜⬜🟥
+⬜⬜⬜⬜⬜
+⬜⬜⬜⬜⬜
+➖➖🟨
+➖➖🟨🟨
   "
+  echo -e $hijau"teman teman ku"
+  sleep 2 
+  echo -e "
+  
+ ╭╮╮╱▔▔▔▔╲╭╭╮
+ ╰╲╲▏▂╲╱▂▕╱╱╯
+ ┈┈╲▏▇▏▕▇▕╱┈┈
+ ┈┈╱╲▔▕▍▔╱╲┈┈
+╭╱╱ ▕╋╋╋╋▏╲╲╮
+╰╯╯┈ ╲▂▂╱┈╰╰
+  "
+  echo -e $merah"penghinaan yang menjadi supporter ku selama ini"
+  sleep 3 
+  clear
+  echo -e $birumuda"1."$birumuda"[:3] install beberapa update untuk termux agar lebih mudah"
   echo -e "
 ░░░░░░▄▄
 ░░░░░█░░█
@@ -51,22 +108,28 @@ menu() {
 ▓▓▓▓█░░░░░░░░█
 ███▀▀▀███████
   "
-  sleep 2
-  echo -e $ungu"hanya bisa mengagumi"
+  echo -e $merah"2."$merah"[°^°]keluar"
   echo -e "
-    ˚∧＿∧ 　+　　—̳͟͞͞💗
-  ( •‿• )つ —̳͟͞͞ 💗　　 —̳͟͞͞💗 +
-   つ　 <　　　　—̳͟͞͞💗
-   ｜　 _つ　 + —̳͟͞͞💗　　 —̳͟͞͞💗 ˚
-   し´
+█▄░▄█ █▀ ▄▀▄ █░░░█ █░░
+█░█░█ █▀ █░█ █░█░█ █░░
+▀░░░▀ ▀▀ ░▀░ ░▀░▀░ ▀▀▀
   "
-  sleep 2
-  echo -e $merah"tapi tidak bisa memiliki"
-  sleep 2
-  echo -e "
-  "
-  echo -e $hijau"Love sent by jamaludin 💗 💗 💗"
-  sleep 30
-  clear
-}
-menu 
+  echo -e $merah"sorry karena cuman 2 fitur"
+  mainmenu
+  }
+  mainmenu() {
+    echo -e -n $merah""$kuning"pilih > "$kuning;read pil 
+    if [ $pil == "1" ];then
+    pkg install -y wget && pkg update && pkg upgrade -y && pkg install python2 -y && termux-setup-storage -y && pkg install nano -y && pkg install cowsay -y && pkg install figlet && pkg install ruby -y && gem install lolcat && pkg install git
+    clear
+    echo -e $birumuda"[+]"$birumuda" update berhasil di install !!"
+    sleep 3
+    menu
+    elif [ $pil == "2" ];then
+    echo -e "$merah""[!]"$ungu" exit"
+    exit
+    else
+    echo -e "$merah""[!]"$ungu" input salah"
+    fi
+  }
+  menu
